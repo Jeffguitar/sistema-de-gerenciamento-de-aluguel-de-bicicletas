4af0eef78bfec50f4cdf542e0a4c861c3ea82aec
Jeffersonnn.PE@gmail.com <Jeffersonnn.PE@gmail.com@918f426b-7153-0d8f-20c9-6542b33993dc>
2014-12-04 01:00:15 +0000

    Ajustes de lógica. Proximo passo é reorganizar os métodos do repositório, finalizar as telas de cadastro, implementar os metodos da fachada e implementar a interface IFachada.

--- trunk/POO - Sistema de Aluguel de Bicicleta/src/br/ufrpe/sistema_de_aluguel_de_bicicleta/negocio/ControladorCliente.java
+++ trunk/POO - Sistema de Aluguel de Bicicleta/src/br/ufrpe/sistema_de_aluguel_de_bicicleta/negocio/ControladorCliente.java
@@ -5,37 +5,37 @@ import br.ufrpe.sistema_de_aluguel_de_bicicleta.negocio.classes_basicas.Cliente;
 
 public class ControladorCliente {
 
-	private RepositorioClienteArray repositorio;
+	private static RepositorioClienteArray repositorio;
 
 	public ControladorCliente() {
-		this.repositorio = RepositorioClienteArray.getInstance();
+		ControladorCliente.repositorio = RepositorioClienteArray.getInstance();
 	}
 
 	public void cadastrar(Cliente usuario) {
 		if (this.procurar(usuario.getCpf()) == null) {
-			this.repositorio.cadastrarUsuario(usuario);
+			ControladorCliente.repositorio.cadastrarUsuario(usuario);
 		}
 	}
 
 	public Cliente procurar(String cpf) {
 		Cliente retorno = null;
 		if (cpf != null) {
-			retorno = this.repositorio.procurarCliente(cpf);
+			retorno = ControladorCliente.repositorio.procurarCliente(cpf);
 		}
 		return retorno;
 	}
 
 	public Cliente atualizarDados(String cpf) {
 		if (cpf != null) {
-			return this.repositorio.alterarCliente(cpf);
+			return ControladorCliente.repositorio.alterarCliente(cpf);
 		} else
 			return null;
 	}
 
 	public void remover(String cpf) {
-		Cliente auxiliar = this.repositorio.procurarCliente(cpf);
+		Cliente auxiliar = ControladorCliente.repositorio.procurarCliente(cpf);
 		if (cpf != null && auxiliar != null) {
-			this.repositorio.excluirUsuario(cpf);
+			ControladorCliente.repositorio.excluirUsuario(cpf);
 		}
 	}
 }
